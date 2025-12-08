1e5fd9721a8e650bc2c09bce67cb6ebbfd46e7a2
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-12-08 15:44:39 +0530

    chore(nix): update flake inputs and configure pre-commit

--- flake/checks.nix
+++ flake/checks.nix
@@ -1,37 +1,42 @@
 {inputs, ...}: {
   imports = [inputs.git-hooks.flakeModule];
 
-  perSystem.pre-commit = {
-    check.enable = true;
+  perSystem = {pkgs, ...}: {
+    pre-commit = {
+      check.enable = true;
 
-    settings = {
-      src = ../.;
-      excludes = ["flake.lock"];
-      default_stages = ["pre-commit"];
-      hooks = {
-        ## Formatting
-        treefmt.enable = false;
-        trim-trailing-whitespace.enable = true;
-        mixed-line-endings.enable = false;
+      settings = {
+        enable = true;
+        package = pkgs.prek;
+        gitPackage = pkgs.gitFull;
+        src = ../.;
+        excludes = ["flake.lock"];
+        default_stages = ["pre-commit"];
+        hooks = {
+          ## Formatting
+          treefmt.enable = false;
+          trim-trailing-whitespace.enable = true;
+          mixed-line-endings.enable = false;
 
-        check-added-large-files = {
-          enable = true;
-          excludes = [
-            ## Images
-            "\\.png"
-            "\\.jpg"
-            "\\.jpeg"
-            "\\.svg"
-            "\\.gif"
-          ];
+          check-added-large-files = {
+            enable = true;
+            excludes = [
+              ## Images
+              "\\.png"
+              "\\.jpg"
+              "\\.jpeg"
+              "\\.svg"
+              "\\.gif"
+            ];
+          };
+          check-case-conflicts.enable = true;
+          check-executables-have-shebangs.enable = true;
+          check-shebang-scripts-are-executable.enable = true;
+
+          fix-byte-order-marker.enable = true;
+          check-merge-conflicts.enable = true;
+          detect-private-keys.enable = true;
         };
-        check-case-conflicts.enable = true;
-        check-executables-have-shebangs.enable = true;
-        check-shebang-scripts-are-executable.enable = true;
-
-        fix-byte-order-marker.enable = true;
-        check-merge-conflicts.enable = true;
-        detect-private-keys.enable = true;
       };
     };
   };
